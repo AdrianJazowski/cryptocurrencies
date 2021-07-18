0b3f340659c3b8cb8de62cbbc5de5949bda8ece9
Adrian Jazowski <adrian.jazowski@gmail.com>
2021-07-18 15:34:15 +0200

    creating logic in app.js, downloading data to start the application, to the initial table and to the input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import React, { useEffect } from "react";
+import Navbar from "./components/navbar/Navbar";
+import MainPage from "./pages/mainPage/MainPage";
+import GlobalStyles from "./globalStyles/GlobalStyles";
+import HeroBanner from "./components/heroBanner/HeroBanner";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getCryptocurrenciesFromApi, getDataForInput } from "./actions";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  const getFirstCryptocurrenciesToMainPage = (quantity) => {
+    const apiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${quantity}`;
+    axios
+      .get(apiUrl, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "X-CMC_PRO_API_KEY": process.env.REACT_APP_API_KEY,
+        },
+      })
+
+      .then((response) => {
+        const cryptocurrencies = response.data.data.map((crypto) => {
+          const { name, symbol } = crypto;
+          const { price, volume_24h, market_cap } = crypto.quote.USD;
+          const newCryptocurrencies = {
+            name: name,
+            symbol: symbol,
+            price: price,
+            volume: volume_24h,
+            marketCap: market_cap,
+          };
+          return newCryptocurrencies;
+        });
+        dispatch(getCryptocurrenciesFromApi(cryptocurrencies));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getDataFromApiToInput = (quantity) => {
+    const apiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${quantity}`;
+    axios
+      .get(apiUrl, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "X-CMC_PRO_API_KEY": process.env.REACT_APP_API_KEY,
+        },
+      })
+      .then((response) => {
+        const cryptoNames = response.data.data.map((crypto) => {
+          const arrayOfCrypto = {
+            name: crypto.name,
+            id: crypto.id,
+          };
+          return arrayOfCrypto;
+        });
+        dispatch(getDataForInput(cryptoNames));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getFirstCryptocurrenciesToMainPage(10);
+    getDataFromApiToInput(500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <Navbar />
+      <HeroBanner />
+      <MainPage />
+    </>
   );
-}
+};
 
 export default App;
